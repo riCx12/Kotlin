60b3ee910aef1b3260780ab533ccd99b6380eda9
riCx12 <victor.r.rici@gmail.com>
2021-04-12 07:51:56 -0300

    Inclusão de conceitos de abstração

--- src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -1,9 +1,10 @@
 package one.digitalinnovation.digionebank
 import java.math.BigDecimal
 
-class Funcionario(
+abstract class Funcionario(
         override val nome: String,
         override val cpf: String,
-        val salario: BigDecimal
+        val salario: BigDecimal,
 ) :Pessoa(nome, cpf) {
- }
+  abstract fun calculoAuxilio():
+}
